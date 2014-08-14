f19067206c90c596917b3c5e170a7f6d0fbd0ea9
Fernando Contreras <luis_fercast@hotmail.com>
2014-08-14 11:12:35 -0500

    Improving the code

--- primo2.py
+++ primo2.py
@@ -1,7 +1,7 @@
 import sys
 def gcd(a,b):
 	(a,b) = (max(a,b),min(a,b))
-	print (a,b)
 	return a if b==0 else gcd(b, a%b)
 
-print gcd(int(sys.argv[1]),int(sys.argv[2]))
+rel = gcd(int(sys.argv[1]),int(sys.argv[2]))
+print "Son realtivamente primos" if rel==1 else "No son realtivamente primos" 
